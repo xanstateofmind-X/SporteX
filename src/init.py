76be5c08809bfeb7f6638dbf327b6acf02ee5756
xanstateofmind-X <sanskriti.birange@gmail.com>
2025-07-14 12:46:55 +0530

    Create init.py

---
+++ src/init.py
@@ -0,0 +1,9 @@
+"""
+Playo Sports Venue Booking Automation
+
+A Python automation script using Playwright to book sports venues on Playo.co
+"""
+
+__version__ = "1.0.0"
+__author__ = "Playo Automation Script"
+__description__ = "Automated sports venue booking for Playo.co"
